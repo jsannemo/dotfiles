6833e6d5b7e1d8efcdc09f0b083e5de368e87c69
Johan Sannemo <jsannemo@jsannemo.se>
2017-08-24 15:55:46 +0200

    Only install blog in home mode

--- home/install.zsh
+++ home/install.zsh
@@ -9,7 +9,10 @@ else
     git clone git@github.com:jsannemo/dotfiles $HOME/Code/dotfiles
 fi
 
-if [[ ! -d cd $HOME/Code/blog ]]
+if want_home 
 then
-    git clone git@github.com:jsannemo/jsannemo.github.io.git $HOME/Code/blog
+    if [[ ! -d cd $HOME/Code/blog ]]
+    then
+        git clone git@github.com:jsannemo/jsannemo.github.io.git $HOME/Code/blog
+    fi
 fi
