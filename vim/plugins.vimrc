60bfe959b7eb5b5591dd81c137723e2e6bdf0070
Johan Sannemo <jsannemo@jsannemo.se>
2023-06-10 09:42:27 +0200

    bump

--- vim/plugins.vimrc
+++ vim/plugins.vimrc
@@ -3,6 +3,4 @@ set rtp+=~/.vim/bundle/Vundle.vim
 call vundle#begin()
 Plugin 'VundleVim/Vundle.vim'
 Plugin 'tomasr/molokai'
-Plugin 'tpope/vim-fugitive'
-Plugin 'tpope/vim-rhubarb'
 call vundle#end()
--- zsh/commands.zsh
+++ zsh/commands.zsh
@@ -1,2 +1,3 @@
 alias open='xdg-open'
 alias c='g++ -Wall -Wconversion -Wfatal-errors -g -std=c++23 -fsanitize=undefined,address'
+alias c='g++ -Wall -Wconversion -Wfatal-errors -g -std=c++20 -fsanitize=undefined,address'
